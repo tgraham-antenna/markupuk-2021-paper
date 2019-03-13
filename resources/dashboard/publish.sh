0b455b052d23ce3369cf44ef7383e53382a7709e
George Bina <george@oxygenxml.com>
2019-03-13 17:32:39 +0200

    Update dashboard template.

--- resources/dashboard/publish.sh
+++ resources/dashboard/publish.sh
@@ -21,3 +21,4 @@ EDIT_URL="https://www.oxygenxml.com/oxygen-xml-web-author/app/oxygen.html?url=gi
 echo "window.REPO_LINK = \"${CLEAN_REPO_URL}\";" > out/params.js
 echo "window.EDIT_LINK = \"${EDIT_URL}\";" >> out/params.js
 cp resources/dashboard/index.html out/index.html
+cp resources/dashboard/site.css out/site.css
