796b4cd66a3ade0f354b6c1a1baafced83628d58
Cristi Talau <cristi_talau@sync.ro>
2019-03-13 16:02:15 +0200

    Switched Netlify publishing to docbook-xsl

--- resources/pdf/publish.sh
+++ resources/pdf/publish.sh
@@ -1,23 +1,32 @@
-SOURCE=paper.xml
+OXY_VERSION="21.0.0.0"
 
 mkdir -p bin
-if [[ ! -e bin/oxygen-pdf-chemistry ]];
+echo "Downloading the DocBook framework"
+if [[ ! -e bin/docbook ]];
 then
-  wget https://www.oxygenxml.com/InstData/Chemistry/oxygen-pdf-chemistry.zip -O bin/oxygen-pdf-chemistry.zip
-  unzip bin/oxygen-pdf-chemistry.zip -d bin/
+  curl -q https://www.oxygenxml.com/maven/com/oxygenxml/frameworks/21.0.0.0/frameworks-21.0.0.0.zip > bin/frameworks.zip
+  unzip -q bin/frameworks.zip -d bin/
 fi
 
-if [[ ! -e bin/frameworks ]];
+echo "Downloading Saxon 6.5.5"
+if [[ ! -e bin/saxon.jar ]];
 then
-  wget https://www.oxygenxml.com/maven/com/oxygenxml/frameworks/21.0.0.0/frameworks-21.0.0.0.zip -O bin/frameworks.zip
-  mkdir bin/frameworks
-  unzip bin/frameworks.zip -d bin/frameworks/
+  curl -q https://packages.atlassian.com/maven-3rdparty/saxon/saxon/6.5.5/saxon-6.5.5.jar > bin/saxon.jar
+fi
+
+echo "Downloading FOP 2.1"
+if [[ ! -e bin/fop-2.1 ]];
+then
+  curl -q http://apache.javapipe.com/xmlgraphics/fop/binaries/fop-2.1-bin.zip > bin/fop.zip
+  unzip -q bin/fop.zip -d bin/
 fi
 
 mkdir -p out
 
-sh "bin/oxygen-pdf-chemistry/chemistry.sh" -in "${SOURCE}" \
-  -css resources/pdf/paper.less bin/frameworks/docbook/css/docbook.css bin/frameworks/docbook/css/print.css \
-  -out "out/paper.pdf"
+echo "Generating output"
+java -cp "bin/saxon.jar:bin/docbook/xsl/extensions/xslthl.jar" com.icl.saxon.StyleSheet paper.xml bin/docbook/xsl/fo/docbook_custom.xsl > out/paper.fo
 
-
+export OS=Linux 
+bash bin/fop-2.1/fop out/paper.fo out/paper.pdf
+rm out/paper.fo
+echo "Done"
